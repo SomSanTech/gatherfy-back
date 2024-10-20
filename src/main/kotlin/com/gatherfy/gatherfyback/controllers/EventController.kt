b672190092659981aeafa6846262f82ca60f7740
sealbb <chonticha.licha@gmail.com>
2024-10-20 23:46:55 +0700

    feat: add vpn file

--- src/main/kotlin/com/gatherfy/gatherfyback/controllers/EventController.kt
+++ src/main/kotlin/com/gatherfy/gatherfyback/controllers/EventController.kt
@@ -3,14 +3,11 @@ package com.gatherfy.gatherfyback.controllers
 import com.gatherfy.gatherfyback.dtos.EventDTO
 import com.gatherfy.gatherfyback.entities.Event
 import com.gatherfy.gatherfyback.services.EventService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("api")
+@CrossOrigin(origins = ["*"])
 class EventController(var eventService: EventService) {
 
     @GetMapping("/v1/events")
