cb6c7f584dd38b1434e52f245af44c63a0065b85
baybiebxm <thaksaporn.xxls@mail.kmutt.ac.th>
2025-02-17 02:47:49 +0700

    fix: edit allowedOrigins

--- src/main/kotlin/com/gatherfy/gatherfyback/configuration/WebMvcConfigurer.kt
+++ src/main/kotlin/com/gatherfy/gatherfyback/configuration/WebMvcConfigurer.kt
@@ -15,7 +15,7 @@ class CorsConfiguration : WebMvcConfigurer {
         val configuration = org.springframework.web.cors.CorsConfiguration()
 
         // อนุญาตเฉพาะ origin ที่กำหนด
-        configuration.allowedOrigins = listOf("http://localhost:3000") // เปลี่ยนเป็น origin จริงเมื่ออยู่ใน production
+        configuration.allowedOrigins = listOf("http://localhost:3000","http://cp24us1.sit.kmutt.ac.th:543","https://capstone24.sit.kmutt.ac.th/us1") // เปลี่ยนเป็น origin จริงเมื่ออยู่ใน production
 
         // อนุญาต credentials (สำหรับส่งคุกกี้, authorization headers, เป็นต้น)
         configuration.allowCredentials = true
